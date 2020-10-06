4c0960c1bc6890c6047536dd656b326950be433b
lecaovan2000 <72064491+lecaovan2000@users.noreply.github.com>
2020-10-06 00:44:43 -0700

    lab-3

---
+++ lab3.java
@@ -0,0 +1,25 @@
+package lecaovan;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class lab3 {
+
+	public static void main(String[] args) {
+		try{
+			
+			InetAddress host = InetAddress.getByName("docs.google.com");
+			String hostName = host.getHostName();
+			System.out.println("Host name:"+hostName);
+			System.out.println("Dia chi IP:"+host.getHostAddress());
+			}
+			catch(UnknownHostException e)
+			{
+			System.out.println("Khong tim thay dia chi");
+			return;
+			}
+
+
+	}
+
+}
